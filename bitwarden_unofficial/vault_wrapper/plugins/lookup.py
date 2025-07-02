873b33380cd24ea8234ee2360991bda98fe5e4d0
Moreno Giussani <giussani.moreno@gmail.com>
2025-07-02 21:05:41 +0200

    feat: add working lookup script

--- bitwarden_unofficial/vault_wrapper/plugins/lookup.py
+++ bitwarden_unofficial/vault_wrapper/plugins/lookup.py
@@ -1,14 +1,19 @@
 # lookup.py
-from __future__ import (absolute_import, division, print_function)
+from __future__ import absolute_import, division, print_function
+
 __metaclass__ = type
 
 import os
+import re
 import subprocess
 import tempfile
-from threading import Lock
 
 from ansible.plugins.lookup import LookupBase
 from ansible.errors import AnsibleError
+from ansible.utils.display import Display
+
+display = Display()
+
 
 class BitwardenCliWrapper:
 
@@ -22,81 +27,133 @@ class BitwardenCliWrapper:
         # Explicitly clean up temporary directory
         self._tempdir_obj.cleanup()
 
-    def get_secret(self, bw_url:str, bw_client_id:str, bw_client_secret:str, bw_password:str, secret_id:str, secret_type:str):
-        env = os.environ.copy()
-        env['HOME'] = self._tmp_dir
-        env['BW_CLIENTID'] = bw_client_id
-        env['BW_CLIENTSECRET'] = bw_client_secret
+    def run(self, command, **kwargs):
+        try:
+            result = subprocess.run(
+                command,
+                env=self._env,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                check=True,
+                text=True,
+                **kwargs,
+            )
+            return result.stdout.strip()
+        except subprocess.CalledProcessError as e:
+            raise RuntimeError(
+                f"Command failed: {' '.join(command)}\n{e.stderr.strip()}"
+            )
 
-        def run(command, **kwargs):
-            try:
-                result = subprocess.run(
-                    command,
-                    env=env,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE,
-                    check=True,
-                    text=True,
-                    **kwargs
-                )
-                return result.stdout.strip()
-            except subprocess.CalledProcessError as e:
-                raise RuntimeError(f"Command failed: {' '.join(command)}\n{e.stderr.strip()}")
+    def get_secret(
+        self,
+        bw_url: str,
+        bw_client_id: str,
+        bw_client_secret: str,
+        bw_password: str,
+        secret_id: str,
+        secret_type: str,
+    ):
 
+        self._env = os.environ.copy()
+        self._env["HOME"] = self._tmp_dir
+        self._env["BW_CLIENTID"] = bw_client_id
+        self._env["BW_CLIENTSECRET"] = bw_client_secret
+
+        display.display("Set url to '" + bw_url + "'")
         # Step 1: Set Bitwarden server - TODO add logging here
-        run(['bw', 'config', 'server', bw_url])
+        self.run(["bw", "config", "server", bw_url])
 
+        display.display("Logging in using api key")
         # Step 2: Log in using the API key (client ID/secret)
-        run(['bw', 'login', '--apikey'])
+        display.display(self.run(["bw", "login", "--apikey"]))
 
         # Step 3: Sync
-        run(['bw', 'sync'])
+        display.display("Syncing the vault...")
+        display.display(self.run(["bw", "sync"]))
 
         # Step 4: Unlock vault
-        run(['bw', 'unlock', bw_password])
+        self._env["BW_PASSWORD"] = bw_password
+        unlock_output = self.run(["bw", "unlock", "--passwordenv", "BW_PASSWORD"])
+        self._env["BW_PASSWORD"] = ""
+
+        regex_session_key = r'export BW_SESSION="([^"]+)"'
+        match = re.search(regex_session_key, unlock_output)
+        if match:
+            self._env["BW_SESSION"] = match.group(1)
+        else:
+            raise AnsibleError(
+                "Unlocking failed, probably a wrong vault password had been provided",
+                obj=unlock_output,
+                show_content=True,
+            )
 
         # Step 5: Read secret value and return it
         # bw get (item|username|password|uri|totp|exposed|attachment|folder|collection|organization|org-collection|template|fingerprint) <id> [options]
-        run(['bw', 'get', secret_type, secret_id])
-        
-
+        secret = self.run(["bw", "get", secret_type, secret_id])
+        display.display("Ssecret: " + secret)
+        return [secret]
 
 
 class BWConfig:
-    MANDATORY_PARAMETERS=[
-        'BW_CLIENT_ID',
-        'BW_CLIENTSECRET',
-        'BW_GRANT_TYPE'
-    ]
-    def __init__(self, **kwargs):
-        self.client_id = kwargs.get('BW_CLIENT_ID', os.getenv('BW_CLIENT_ID'))
-        self.scope = kwargs.get('BW_SCOPE', os.getenv('BW_SCOPE'))
-        self.grant_type = kwargs.get('BW_GRANT_TYPE', os.getenv('BW_GRANT_TYPE'))
-        self.url = kwargs.get('BW_URL', os.getenv('BW_URL'))
-        self.client_secret = kwargs.get('BW_CLIENTSECRET', os.getenv('BW_CLIENTSECRET')),
-        self.vault_password = kwargs.get('BW_VAULT_PASSWORD')
+    MANDATORY_PARAMETERS = ["BW_CLIENT_ID", "BW_CLIENTSECRET", "BW_GRANT_TYPE"]
 
-        missing = [k for k, v in self.__dict__.items() if v is None and v not in BWConfig.MANDATORY_PARAMETERS]
+    def __init__(self, **kwargs):
+        self.client_id = kwargs.get("BW_CLIENT_ID", os.getenv("BW_CLIENT_ID"))
+        self.scope = kwargs.get("BW_SCOPE", os.getenv("BW_SCOPE"))
+        self.grant_type = kwargs.get("BW_GRANT_TYPE", os.getenv("BW_GRANT_TYPE"))
+        self.url = kwargs.get("BW_URL", os.getenv("BW_URL"))
+        self.client_secret = kwargs.get("BW_CLIENTSECRET", os.getenv("BW_CLIENTSECRET"))
+        missing = [
+            k
+            for k, v in self.__dict__.items()
+            if v is None and v not in BWConfig.MANDATORY_PARAMETERS
+        ]
         if missing:
-            raise ValueError(f"Missing required configuration values: {', '.join(missing)}")
+            raise ValueError(
+                f"Missing required configuration values: {', '.join(missing)}"
+            )
 
 
 class LookupModule(LookupBase):
-    ALLOWED_LOOKUP_TYPES=["item","username","password","uri","totp","exposed","attachment","folder","collection","organization","org-collection","template","fingerprint"]
-    
+    ALLOWED_LOOKUP_TYPES = [
+        "item",
+        "username",
+        "password",
+        "uri",
+        "totp",
+        "exposed",
+        "attachment",
+        "folder",
+        "collection",
+        "organization",
+        "org-collection",
+        "template",
+        "fingerprint",
+    ]
+
     def run(self, terms, variables=None, **kwargs):
-        if len(terms) != 2:
-            raise AnsibleError("You must provide the type and the id to lookup")
+        if len(terms) != 3:
+            raise AnsibleError(
+                "You must provide the type, id to lookup and the vault password"
+            )
         secret_type = terms[0]
         if secret_type not in LookupModule.ALLOWED_LOOKUP_TYPES:
-            raise AnsibleError("You must provide a valid type between item|username|password|uri|totp|exposed|attachment|folder|collection|organization|org-collection|template|fingerprint")
+            raise AnsibleError(
+                "You must provide a valid type between item|username|password|uri|totp|exposed|attachment|folder|collection|organization|org-collection|template|fingerprint"
+            )
         secret_id = terms[1]
-        
+        vault_password = terms[2]
+
         # Load configuration from kwargs or fallback to environment variables
         config = BWConfig(**kwargs)
 
         bw_cli = BitwardenCliWrapper()
 
-        return bw_cli.get_secret(config.url,config.client_id,config.client_secret,config.vault_password,secret_id,secret_type)
-
-
+        return bw_cli.get_secret(
+            config.url,
+            config.client_id,
+            config.client_secret,
+            vault_password,
+            secret_id,
+            secret_type,
+        )
